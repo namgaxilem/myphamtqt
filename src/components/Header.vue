a1674ba3ff62bb7e60c8a2a6d2ddf0946a579ca6
namgaxilem <namgaxilem@gmail.com>
2020-03-16 22:53:30 +0700

    athome commit

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-row>
+      <b-col
+        lg="4"
+        md="12"
+      >
+      </b-col>
+      <b-col
+        lg="4"
+        md="12"
+      >
+      <div class="banner"> 
+        <p>MỸ PHẨM TQT</p>
+        <p>CHUYÊN SỈ, LẺ CÁC LOẠI</p>
+        <p>SẢN PHẨM TÓC</p>
+      </div>
+      </b-col>
+      <b-col
+        lg="4"
+        md="12"
+      >
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.banner {
+  border: 1px solid black;
+}
+</style>
